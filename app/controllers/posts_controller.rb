ca4411c55eea03289bc6f0db26c12fa1bc428e4d
Bruno Brito <brunofbrito@gmail.com>
2015-09-01 17:19:31 +0100

    Delete post button works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,12 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
     # Using a private method to encapsulate the permissible parameters is
     # just a good pattern since you'll be able to reuse the same permit
