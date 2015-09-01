cae9d3e7b7e327797508a79c1686c2b9ca6ef001
Bruno Brito <brunofbrito@gmail.com>
2015-09-01 16:25:03 +0100

    adds Post database

---
+++ db/migrate/20150901151759_add_tables_to_posts.rb
@@ -0,0 +1,4 @@
+class AddTablesToPosts < ActiveRecord::Migration
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+posts = Post.create([title: "Hello World", description: "An amazing story, this one. Read all about it after I finish True Detective season 2!"])
+posts = Post.create([title: "Goodbye Cruel World", description: "Eric Prydz is the man to beat, Ibiza is the place to be. Your time is up, his time is now!"])
