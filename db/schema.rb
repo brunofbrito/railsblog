25c35905a3ba0212598131c21f7e7f150124494c
Bruno Brito <brunofbrito@gmail.com>
2015-09-01 19:16:13 +0100

    Adds Create and Delete comments on single post page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901155335) do
+ActiveRecord::Schema.define(version: 20150901163254) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at",  null: false
