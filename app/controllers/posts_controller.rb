1247bcff458ea5a9ec538e95fbf9f93251239dee
Bruno Brito <brunofbrito@gmail.com>
2015-09-01 17:17:39 +0100

    Adds Edit and Delete buttons if user is author

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to root_path
+  end
+
+  private
+    # Using a private method to encapsulate the permissible parameters is
+    # just a good pattern since you'll be able to reuse the same permit
+    # list between create and update. Also, you can specialize this method
+    # with per-user checking of permissible attributes.
+    def post_params
+      params.require(:post).permit(:title, :description)
+    end
 end
