0156bb6e73d0b13cbcc7b611e9356d20cc2c651c
Bruno Brito <brunofbrito@gmail.com>
2015-09-01 17:27:37 +0100

    Edit post feature now works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,16 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
