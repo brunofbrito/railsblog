5715ab7cbb5c8c3244e3acbeb21a19b66039c4e9
Bruno Brito <brunofbrito@gmail.com>
2015-09-03 02:07:00 +0100

    Adds styling to show post page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,28 @@ module ApplicationHelper
   def background
     background = Settings.find(1).bground.url
   end
+
+  def facebook
+    Settings.find(1).facebook
+  end
+
+  def twitter
+    Settings.find(1).twitter
+  end
+
+  def instagram
+    Settings.find(1).instagram
+  end
+
+  def youtube
+    Settings.find(1).youtube
+  end
+
+  def linkedin
+    Settings.find(1).linkedin
+  end
+
+  def github
+    Settings.find(1).github
+  end
 end
