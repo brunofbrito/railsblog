9a8a0e67ea2148d7aea02ee2d1bc3b47c5a5200e
Bruno Brito <brunofbrito@gmail.com>
2015-09-05 17:59:41 +0100

    Adds pagination to index and author pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def index
     @blog = Settings.find(1)
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.paginate(:page => params[:page]).order(created_at: :desc)
   end
 
   def show
